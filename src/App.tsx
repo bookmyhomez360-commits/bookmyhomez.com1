710bea98c9bad69c0d9152238e4638c45dfd5f73
bookmyhomez360-commits <bookmyhomez360@gmail.com>
2026-08-03 23:37:49 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { PropertyDetailsModal } from './components/PropertyDetailsModal';
 import  WizardModal  from './components/WizardModal';
 import { AuthModal } from './components/AuthModal';
 import { Footer } from './components/Footer';
-import { Chatbot } from './components/Chatbot';
 import {
   Property,
   User,
@@ -973,9 +972,6 @@ export default function App() {
           navigateToCategory={navigateToCategory}
           filterByLocation={filterByLocation}
         />
-
-        {/* Custom BookMyHomez Chatbot Widget */}
-        <Chatbot />
 
       </div>
     </div>
